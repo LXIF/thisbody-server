895a3335e2fccfaa0182d7ce588ca38ba6aca02c
LXIF <andri.schatz@gmail.com>
2021-10-22 20:46:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//jshint: esversion 6
+
+const serveStatic = require('serve-static');
+const express = require('express');
+const app = express();
+app.use(express.static("public"));
+
+// app.get('', (req, res) => {
+//     res.sendFile("index.html")
+// });
+
+var history = require('connect-history-api-fallback');
+
+app.use(history());
+app.use(serveStatic(__dirname + '/dist'));
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+
+    //handshakeish
+
+    socket.on('howdy', (message) => {
+
+        console.log(message);
+
+    });
+
+    //local presence
+
+    socket.on('breath', (breath) => {
+        socket.broadcast.emit('breath', breath);
+    });
+    socket.on('bite', (bite) => {
+        socket.broadcast.emit('bite', bite);
+    });
+    socket.on('nod', (nod) => {
+        socket.broadcast.emit('nod', nod);
+    });
+    socket.on('tilt', (tilt) => {
+        socket.broadcast.emit('tilt', tilt);
+    });
+    socket.on('heartbeat', (heartbeat) => {
+        socket.broadcast.emit('heartbeat', heartbeat);
+    });
+});
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log('listnin 3K yeet');
+});
