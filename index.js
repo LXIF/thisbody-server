8c04eee3c34a2615559149134f37d0d73e3f545e
LXIF <andri.schatz@gmail.com>
2021-10-23 16:44:17 +0200

    noww pwease?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const serveStatic = require('serve-static');
 const express = require('express');
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // app.get('', (req, res) => {
 //     res.sendFile("index.html")
